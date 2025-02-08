e404248c991197d11de9dd0d62f039604ed449cd
SosoTlm2 <sosozouytb@gmail.com>
2025-02-08 21:33:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 
 
-  for (const [selector, translationKey] of Object.entries(elementsToTranslate)) {
-    const element = document.querySelector(selector);
-    if (element) {
-      element.textContent = translations[userLang][translationKey];
-    }
-  }
-}
-
 // Apply translations on page load
 window.addEventListener('DOMContentLoaded', applyTranslations);
